0b0fbc011ff4d8c5ee922f382d12cb3520bdd88e
tujiawei <you@example.com>
2022-02-25 16:40:33 +0800

    refactor(install.sh): 新加一个安装脚本

---
+++ scripts/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Filename: ~/.vim/scripts/install.sh
+#Author: Mr Prince
+#Date: 2022-02-25 10:45:19
+# 初始安装脚本
+git clone https://github.com/mrtujiawei/vimrc.git --depth 1 ~/.vim
+
+# 放在下面的原因是安装的时候会进入vim
+# 后面的脚本就执行不下去了
+# 安装vundle vim包管理工具
+git clone https://github.com/VundleVim/Vundle.vim.git --depth 1 ~/.vim/bundle/Vundle.vim
+
+# 失败几率比较大，所以重试两次
+vim +PluginInstall +qall && vim +PluginInstall +qall && vim +PluginInstall +qall && vim -c ':call mkdp#util#install()' +qall
+
+# coc-nvim 需要手动更新一下
+cd ~/.vim/bundle/coc.nvim/
+yarn
+cd -
+
+# vim-jsdoc 需要手动安装一下
+cd ~/.vim/bundle/vim-jsdoc/
+make install
+cd -
