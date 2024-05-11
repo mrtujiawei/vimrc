7f159bf2458710fda730fb44da20ad870dd85957
tujiawei <jiaweitu@marcothepolo.com>
2024-05-11 17:17:30 +0800

    docs(onmyzsh.sh): onmyzsh config

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -48,3 +48,61 @@ bash ohmyzsh/tools/install.sh
 # then
 #   tmux
 # fi
+#
+
+# fzf 配置
+# export FZF_COMPLETION_TRIGGER='**'
+#
+# # Options to fzf command
+# export FZF_COMPLETION_OPTS='--border --info=inline'
+#
+# # Use fd (https://github.com/sharkdp/fd) instead of the default find
+# # command for listing path candidates.
+# # - The first argument to the function ($1) is the base path to start traversal
+# # - See the source code (completion.{bash,zsh}) for the details.
+# _fzf_compgen_path() {
+#   fd --hidden --follow --exclude ".git" . "$1"
+# }
+#
+# # Use fd to generate the list for directory completion
+# _fzf_compgen_dir() {
+#   fd --type d --hidden --follow --exclude ".git" . "$1"
+# }
+#
+# # Advanced customization of fzf options via _fzf_comprun function
+# # - The first argument to the function is the name of the command.
+# # - You should make sure to pass the rest of the arguments to fzf.
+# _fzf_comprun() {
+#   local command=$1
+#   shift
+#
+#   case "$command" in
+#     cd)           fzf --preview 'tree -C {} | head -200'   "$@" ;;
+#     export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
+#     ssh)          fzf --preview 'dig {}'                   "$@" ;;
+#     *)            fzf --preview 'bat -n --color=always {}' "$@" ;;
+#   esac
+# }
+#
+
+# # pnpm
+# export PNPM_HOME="/Users/tujiawei/Library/pnpm"
+# case ":$PATH:" in
+#   *":$PNPM_HOME:"*) ;;
+#   *) export PATH="$PNPM_HOME:$PATH" ;;
+# esac
+# # pnpm end
+#
+# # android 配置
+# export ANDROID_HOME=$HOME/Library/Android/sdk
+# export PATH=$PATH:$ANDROID_HOME/emulator
+# export PATH=$PATH:$ANDROID_HOME/platform-tools
+# eval "$(rbenv init - zsh)"
+#
+# # 快速切换路径
+# setopt auto_cd
+# cdpath=($HOME/work $HOME/.vim $HOME/.vim $HOME/github)
+#
+# # maven
+# export PATH=$PATH:$HOME/.maven/bin
+
