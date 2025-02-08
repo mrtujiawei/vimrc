6d11c3b06eee667deba75ef025ac19c18073d544
tujiawei <jiaweitu@marcothepolo.com>
2025-02-08 10:09:48 +0800

    refactor(copilot.lua): close copilot

--- config/plugins/copilot.lua
+++ config/plugins/copilot.lua
@@ -5,6 +5,11 @@
 -- Maintainer: mrtujiawei <m15988803646@163.com>
 -- Last Change: 2025-02-07
 
+-- 关闭
+-- vim.g.copilot_filetypes = {
+--   ['*'] = false,
+-- }
+
 vim.g.copilot_no_tab_map = true
 vim.keymap.set('i', '<C-\\>', 'copilot#Accept("\\<CR>")', {
   expr = true,
